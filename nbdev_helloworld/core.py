f1d072387536447d383bbab96bb0bef474e1fe12
Ramnath <ramnathbabutj@gmail.com>
2022-09-03 05:08:46 +0530

    More changes

--- nbdev_helloworld/core.py
+++ nbdev_helloworld/core.py
@@ -6,7 +6,7 @@ __all__ = ['say_hello', 'foo']
 # %% ..\00_core.ipynb 2
 def say_hello(to):
     "Say hello to somebody"
-    return f'Hello {to}!'
+    return f"Hello {to}!"
 
 # %% ..\00_core.ipynb 6
 def foo(): pass
